d1d28f609ee453e590b831bcd2cba40da55d9faa
DengYiping <Scott@Scotts-MacBook-Pro.local>
2015-09-12 18:31:18 -0500

    Initial Commit

---
+++ qqlogin/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  qqlogin
+//
+//  Created by Scott Deng on 9/12/15.
+//  Copyright (c) 2015 github. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
